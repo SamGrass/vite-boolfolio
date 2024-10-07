acff7a62b4ba337a9cf87ffdf5562e420ed164b4
Samgrass <samuelegrassetti01@gmail.com>
2024-10-07 14:35:21 +0200

    feat: projects page with call to backend apis

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,13 +1,46 @@
 <script>
+import axios  from 'axios';
+import { store } from '../store'
+import Loading from './components/Loading.vue';
+
   export default {
-    name: 'projects'
+    name: 'projects',
+    components: {
+      Loading
+    },
+    data() {
+      return {
+        projects: [],
+        isLoading: false,
+      }
+    },
+    methods: {
+      getApi(){
+        this.isLoading = true;
+        axios.get(store.apiURL + 'projects')
+        .then(res => {
+          this.isLoading = false
+          this.projects = res.data.projects.data;
+          
+        })
+      }
+    },
+    mounted() {
+      this.getApi();
+    },
   }
 </script>
 
 <template>
-  Progetti
+  <h2>PROGETTI</h2>
+  <ul v-if="!isLoading">
+    <li v-for="project in projects">{{ project.name }}</li>
+  </ul>
+  <div v-else="isLoading"><Loading/></div>
 </template>
 
-<style>
-
+<style lang="scss" scoped>
+h2 {
+  margin-bottom: 2rem;
+}
 </style>
